3e2b41e503370c412a3c3d88782b8652adcd0f3d
Joel Leupp <joelleupp@gmail.com>
2017-12-06 08:30:41 +0100

    added task 1

---
+++ task 1/student.py
@@ -0,0 +1,35 @@
+from module import *
+from moduleElement import *
+
+class Student(object):
+
+    def __init__(self, name):
+        self.name=name
+        self.modules=[]
+        self.grades={}
+
+    def add_module(self,module):
+        self.modules.append(module)
+        self.grades[module.get_title()]=module.get_grade()
+    def get_list_modules(self):
+        for i in self.modules:
+            print(i)
+    def get_grades(self):
+
+        for key, value in self.grades.items():
+            print(key, ":" ,value)
+
+
+### test cases ###
+
+me = Student("FirstName LastName")
+me.add_module(info1)
+me.get_list_modules()
+# expected output:
+# Modules of Student FirstName LastName:
+#	Info 1
+
+me.get_grades()
+# expected output:
+# Grades of Student FirstName LastName:
+#	Info 1: 6
